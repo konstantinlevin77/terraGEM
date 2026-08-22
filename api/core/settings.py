11d6d6a742fb7fd15c384d38471a5e18b1798ed8
Mehmet <tekmanmehmetyasar@protonmail.com>
2026-08-22 21:26:52 +0300

    feat: add session authentication
    Enable session authentication to authenticate when using API on the browser.

--- api/core/settings.py
+++ api/core/settings.py
@@ -131,6 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
